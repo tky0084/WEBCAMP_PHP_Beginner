c9eb0441380eb7a22b26182482ddd74e0f4c9807
tky0084 <tky0084@example.com>
2022-06-19 13:14:42 +0000

    define.phpを書いた

---
+++ define.php
@@ -0,0 +1,10 @@
+<?php
+
+/*定数の定義*/
+define('DEF_INT' , 123); //整数
+define('DEF_FLOAT' , 3.14); //少数
+define('DEF_STRING', "string"); //文字列
+
+var_dump(DEF_INT);
+var_dump(DEF_FLOAT);
+var_dump(DEF_STRING);
